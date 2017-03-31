61ec0e0e66c81498a7db4bde2b72297999fd9a25
PanLiyang <liyang.pan@eayun.com>
2017-03-31 19:01:35 +0800

    Restart service when upgrade engine
    Signed-off-by: PanLiyang <liyang.pan@eayun.com>

--- otopi/emdsetup.py
+++ otopi/emdsetup.py
@@ -5,6 +5,7 @@ import os
 from otopi import plugin, util
 
 from ovirt_engine_setup import constants as osetupcons
+from ovirt_engine_setup.engine import constants as oenginecons
 
 
 @util.export
@@ -16,7 +17,22 @@ class Plugin(plugin.PluginBase):
         after=(
             osetupcons.Stages.DIALOG_TITLES_E_SUMMARY,
         ),
+        condition=lambda self: (
+            self.environment[oenginecons.EngineDBEnv.NEW_DATABASE]
+        ),
     )
     def enable_engine_manage_domains_plugin(self):
         os.system("engine-manage-domains-setup")
         self.dialog.note(text="engine manage domains enabled.")
+
+    @plugin.event(
+        stage=plugin.Stages.STAGE_CLOSEUP,
+        after=(
+            osetupcons.Stages.DIALOG_TITLES_E_SUMMARY,
+        ),
+        condition=lambda self: (
+            not self.environment[oenginecons.EngineDBEnv.NEW_DATABASE]
+        ),
+    )
+    def restart_engine_manage_domains_plugin(self):
+        os.system("service engine-manage-domains restart")
